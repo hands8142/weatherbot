c76b6c4a5a3db1f814ed75ab44da3faddd10f764
hands8142 <46277532+hands8142@users.noreply.github.com>
2021-01-20 21:35:30 +0900

    log

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -134,9 +134,9 @@ export default async ({ weather, news, date, url }: discordArgs) => {
   });
 
   // await axios.post(url, message);
-  console.log(message2.embed[0])
+  console.log(message2.embeds[0])
   await axios.post(url, message2);
-  console.log(message3)
+  console.log(message3.embeds[0])
   await axios.post(url, message3);
 };
 
